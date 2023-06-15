b3379c7fae0c39ae9d3a4a9146b803281112a8fe
Ronel028 <floridaronel15@gmail.com>
2023-06-15 21:32:55 +0800

    initial commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import Link from "next/link";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+  Card,
+  IconButton,
+} from "@material-tailwind/react";
+import {
+  CubeTransparentIcon,
+  UserCircleIcon,
+  CodeBracketSquareIcon,
+  Square3Stack3DIcon,
+  ChevronDownIcon,
+  Cog6ToothIcon,
+  InboxArrowDownIcon,
+  LifebuoyIcon,
+  PowerIcon,
+  RocketLaunchIcon,
+  Bars2Icon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+
+// profile menu component
+const profileMenuItems = [
+  {
+    label: "Sign Out",
+    icon: PowerIcon,
+  },
+];
+
+const ProfileMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const closeMenu = () => setIsMenuOpen(false);
+
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+      <MenuHandler>
+        <Button
+          variant="text"
+          color="blue-gray"
+          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+        >
+          <Avatar
+            variant="circular"
+            size="sm"
+            alt="candice wu"
+            className="border border-blue-500 p-0.5"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+          />
+          <ChevronDownIcon
+            strokeWidth={2.5}
+            className={`h-3 w-3 transition-transform ${
+              isMenuOpen ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+      <MenuList className="p-1">
+        {profileMenuItems.map(({ label, icon }, key) => {
+          const isLastItem = key === profileMenuItems.length - 1;
+          return (
+            <MenuItem
+              key={label}
+              onClick={closeMenu}
+              className={`flex items-center gap-2 rounded ${
+                isLastItem
+                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                  : ""
+              }`}
+            >
+              {React.createElement(icon, {
+                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                strokeWidth: 2,
+              })}
+              <Typography
+                as="span"
+                variant="small"
+                className="font-normal"
+                color={isLastItem ? "red" : "inherit"}
+              >
+                {label}
+              </Typography>
+            </MenuItem>
+          );
+        })}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default function Navigation() {
+  return (
+    <Navbar className="mx-auto p-2 border-r-0 shadow-md lg:pl-6">
+      <div className="relative mx-auto flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
+        >
+          KeepNotes
+        </Typography>
+        <div className="flex items-center gap-5">
+          <Link href="/add-notes" className="text-blue-gray-900">
+            <PlusCircleIcon className="h-6 w-6" />
+          </Link>
+          <ProfileMenu />
+        </div>
+      </div>
+    </Navbar>
+  );
+}
---
+++ src/pages/signup.js
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+
+const Signup = () => {
+  return (
+    <div className="w-full h-auto min-h-screen p-4 flex items-center justify-center">
+      <Card shadow={true} className="p-5 w-full sm:w-96 flex justify-center">
+        <Typography variant="h5" color="blue-gray">
+          Creating an Account
+        </Typography>
+        <Typography color="gray" className="mt-1 font-normal text-xs">
+          Enter your details to register.
+        </Typography>
+        <form className="mt-8 mb-2 max-w-screen-lg w-full">
+          <div className="mb-4 flex flex-col gap-6">
+            <Input size="lg" label="Username" />
+            <Input size="lg" label="Email" />
+            <Input type="password" size="lg" label="Password" />
+          </div>
+          <Button
+            className="mt-6 text-xs bg-indigo-500 hover:shadow-indigo-300"
+            fullWidth
+          >
+            Create account
+          </Button>
+          <Typography
+            color="gray"
+            className="mt-4 text-center font-normal text-sm"
+          >
+            Already have an account?
+            <Link
+              href="/signin"
+              className="font-medium text-blue-500 transition-colors hover:text-blue-700"
+            >
+              Sign In
+            </Link>
+          </Typography>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default Signup;
