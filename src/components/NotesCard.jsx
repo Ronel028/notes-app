a3f05ba1e68caf980020ee8cad5b9e00c8e02b77
Ronel028 <floridaronel15@gmail.com>
2023-06-19 22:19:52 +0800

    setup signup page to connect to the server

--- src/components/NotesCard.jsx
+++ src/components/NotesCard.jsx
@@ -38,23 +38,6 @@ const NotesCard = () => {
         </Link>
       </CardFooter>
     </Card>
-    // <Card className="mt-6 w-full">
-    //   <CardBody className="flex-1">
-    //     <Typography variant="h5" color="blue-gray" className="mb-2">
-    //       UI/UX Review Check
-    //     </Typography>
-    //     <Typography>
-    //       The place is close to Barceloneta Beach and bus stop just 2 min by
-    //       walk and near to &quot;Naviglio&quot; where you can enjoy the main
-    //       night life in Barcelona.
-    //     </Typography>
-    //   </CardBody>
-    //   <CardFooter className="pt-0">
-    //     <Button size="sm" className="bg-indigo-500 hover:shadow-indigo-300">
-    //       Read More
-    //     </Button>
-    //   </CardFooter>
-    // </Card>
   );
 };
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import axios from "axios";
 import Link from "next/link";
 import {
   Card,
@@ -8,6 +10,35 @@ import {
 } from "@material-tailwind/react";
 
 const Signup = () => {
+  const [userInput, setUserInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  // get user input value and save to userInput state
+  const getUserInput = (e) => {
+    setUserInput({
+      ...userInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // sumbit userInput to server
+  const saveUser = async (e) => {
+    e.preventDefault();
+    const save = await axios.post(
+      "http://localhost:8000/api/insert-user",
+      userInput,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(save);
+  };
+
   return (
     <div className="w-full h-auto min-h-screen p-4 flex items-center justify-center">
       <Card shadow={true} className="p-5 w-full sm:w-96 flex justify-center">
@@ -17,13 +48,30 @@ const Signup = () => {
         <Typography color="gray" className="mt-1 font-normal text-xs">
           Enter your details to register.
         </Typography>
-        <form className="mt-8 mb-2 max-w-screen-lg w-full">
+        <form className="mt-8 mb-2 max-w-screen-lg w-full" onSubmit={saveUser}>
           <div className="mb-4 flex flex-col gap-6">
-            <Input size="lg" label="Username" />
-            <Input size="lg" label="Email" />
-            <Input type="password" size="lg" label="Password" />
+            <Input
+              size="lg"
+              label="Username"
+              name="username"
+              onChange={getUserInput}
+            />
+            <Input
+              size="lg"
+              label="Email"
+              name="email"
+              onChange={getUserInput}
+            />
+            <Input
+              type="password"
+              size="lg"
+              label="Password"
+              name="password"
+              onChange={getUserInput}
+            />
           </div>
           <Button
+            type="submit"
             className="mt-6 text-xs bg-indigo-500 hover:shadow-indigo-300"
             fullWidth
           >
